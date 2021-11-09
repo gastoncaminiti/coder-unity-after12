e2e652a72db0bc2bf5cc31d7e1ab9f4906ff348b
Gaston Caminiti <gastoncaminiti@gmail.com>
2021-11-09 17:08:41 -0300

    Commit 3 [PRACTICA EN VIVO]
    Goal
    Trigger
    Compound Colliders
    Wheels
    Layer

--- AFTER - FORCEMODE Y LAYERS/Assets/Scripts/PlayerController.cs
+++ AFTER - FORCEMODE Y LAYERS/Assets/Scripts/PlayerController.cs
@@ -5,14 +5,51 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    [SerializeField] private float horizontalInput;
+    [SerializeField] private float forwardInput;
+    [SerializeField] private float horsePower = 10.0f;
+    [SerializeField] private float turnSpeed = 20.0f;
+
+    [SerializeField] WheelCollider[] allWheels;
+
+    private Rigidbody playerRb;
+
+
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        if (IsOnGround()) { 
+            MoveCar();
+        }
+    }
+
+    private void MoveCar()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        playerRb.AddRelativeForce(Vector3.back * horsePower * forwardInput, ForceMode.Acceleration);
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+    }
+
+
+
+    private bool IsOnGround()
+    {
+        if (!allWheels[0].isGrounded) return false;
+        if (!allWheels[1].isGrounded) return false;
+        if (!allWheels[2].isGrounded) return false;
+        if (!allWheels[3].isGrounded) return false;
+        return true;
     }
 }
